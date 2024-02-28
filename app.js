89258a9fd20597cdc2c690df4ae87dcd28e5fc57
Joseph Meza <joseph.meza@unmsm.edu.pe>
2024-02-28 16:33:53 -0500

    db postgres

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 import express, { json } from 'express'
-import { moviesRouter } from './routes/movies.js'
+import { createMovieRouter } from './routes/movies.js'
 import { corsMiddleware } from './middlewares/cors.js'
 // Otra forma de leer un json
 // import { createRequire } from 'node:module'
 // const require = createRequire(import.meta.url)
 // const movies = require('./movies.json')
 
-const app = express()
-app.use(json())
-app.use(corsMiddleware())
-app.disable('x-powered-by')
+export const createApp = ({ movieModel }) => {
+  const app = express()
+  app.use(json())
+  app.use(corsMiddleware())
+  app.disable('x-powered-by')
 
-// metodos normales: GET / HEAD / POST
-// metodos complejos: PUT / PATCH / DELETE
-app.use('/movies', moviesRouter)
+  // metodos normales: GET / HEAD / POST
+  // metodos complejos: PUT / PATCH / DELETE
+  app.use('/movies', createMovieRouter({ movieModel }))
 
-const PORT = process.env.PORT ?? 1234
+  const PORT = process.env.PORT ?? 1234
 
-app.listen(PORT, () => {
-  console.log(`server listening on port http://localhost:${PORT}`)
-})
+  app.listen(PORT, () => {
+    console.log(`server listening on port http://localhost:${PORT}`)
+  })
+}
---
+++ models/postgre/movie.js
@@ -0,0 +1,118 @@
+import pgPromise from 'pg-promise'
+
+const pgp = pgPromise()
+
+const dbConfig = {
+  host: 'localhost',
+  user: 'postgres',
+  port: 5432,
+  password: '123456',
+  database: 'moviesdb'
+}
+
+const client = pgp(dbConfig)
+
+export class MovieModel {
+  static async getAll ({ genre }) {
+    const movies = await client.query(
+      'SELECT * FROM movie'
+    )
+
+    return movies
+  }
+
+  static async getById ({ id }) {
+    const movie = await client.query(
+      'SELECT * FROM movie WHERE id = $1',
+      [id]
+    )
+
+    if (movie.length === 0) return null
+
+    return movie
+  }
+
+  static async create ({ input }) {
+    const {
+      genre: genreInput,
+      title,
+      year,
+      duration,
+      director,
+      rate,
+      poster
+    } = input
+
+    const uuidResult = await client.query('SELECT uuid_generate_v4()')
+    const uuid = uuidResult[0].uuid_generate_v4
+
+    try {
+      // Insertar película en la tabla movie
+      await client.query(
+            `INSERT INTO movie (id, title, year, director, duration, poster, rate)
+              VALUES ($1, $2, $3, $4, $5, $6, $7);`,
+            [uuid, title, year, director, duration, poster, rate]
+      )
+
+      const genreIds = []
+      for (const genreName of genreInput) {
+        const genreQuery = await client.query(
+          'SELECT id FROM genre WHERE LOWER(name) = LOWER($1)',
+          [genreName]
+        )
+        console.log(genreQuery)
+        if (genreQuery.length > 0) {
+          genreIds.push(genreQuery[0].id)
+        } else {
+          console.error(`Genre '${genreName}' not found in the database.`)
+        }
+      }
+
+      // Insertar los géneros en la tabla movie_genres
+      for (const genreId of genreIds) {
+        await client.query(
+          'INSERT INTO movie_genres (movie_id, genre_id) VALUES ($1, $2)',
+          [uuid, genreId]
+        )
+      }
+    } catch (e) {
+      console.error('Error creating movie:', e)
+      throw new Error('Error creating movie')
+    }
+
+    const movie = await client.query(
+      'SELECT * FROM movie WHERE id = $1',
+      [uuid]
+    )
+
+    return movie
+  }
+
+  static async delete ({ id }) {
+    try {
+      // Eliminar las filas relacionadas en la tabla movie_genres
+      await client.query(
+        'DELETE FROM movie_genres WHERE movie_id = $1',
+        [id]
+      )
+
+      // Luego eliminar la fila de la tabla movie
+      const result = await client.query(
+        'DELETE FROM movie WHERE id = $1',
+        [id]
+      )
+
+      // Verificar si se eliminó con éxito la película
+      const success = result.length > 0
+
+      return success
+    } catch (error) {
+      console.error('Error deleting movie:', error)
+      throw new Error('Error deleting movie')
+    }
+  }
+
+  static async update ({ id, input }) {
+
+  }
+}
